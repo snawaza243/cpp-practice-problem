65996393e1b73021d308f4bb82c59b2147d03825
Shahnawaz Alam <snawaza243@gmail.com>
2022-11-14 07:38:43 +0530

    Update cpp

--- 10QArrayIntesection.cpp
+++ 10QArrayIntesection.cpp
@@ -1,14 +1,34 @@
 // intersection or common element
-// if not common then return -1 
+// if not common then return -1
 // check if match then print and update value and after complete break
 
-#include<iostream>
+#include <iostream>
+
+#include <bits/stdc++.h>
 using namespace std;
-void arrayIntersect(){
-    
+vector<int> findArrayIntersection(vector<int> &arr1, int n, vector<int> &arr2, int m)
+{
+    vector<int> ans;
+    int i = 0, j = 0;
+    while (i < n && j < m)
+    {
+        if (arr1[i] == arr2[j])
+        {
+            ans.push_back(arr1[i]);
+            i++;
+            j++;
+        }
+        else if (arr1[i] < arr2[j])
+        {
+            i++;
+        }
+        else
+        {
+            j++;
+        }
+    }
 }
 
-int main(){
-    
-
+int main()
+{
 }
